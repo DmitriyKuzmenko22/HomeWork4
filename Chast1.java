d725b8d55c36200b914bdf1f7819870451735abb
DmitriyKuzmenko22 <dp190792kdg@gmail.com>
2016-11-19 16:18:16 +0200

    Update Chast1.java

--- Chast1.java
+++ Chast1.java
@@ -1,95 +1,94 @@
 import java.util.Arrays;
 import java.util.Collections;
 
-public class DZ {
 
+public class DzNew4 {
     public static void main(String[] args) {
+        int[] arr = new int[2];
+        //2
+        byte[] arr2 = new byte[26];
 
-        int[] arr;
-
-        //Объявите массив byte с идентификатором byteArray на 26 элементов.
-        byte[] byteArray = new byte[26];
-
-// Объявите массив строк с идентификатором stringArray на 10 элементов.
-        //Составить программу вывода на экран любого элемента массива по его индексу.
+        //4
+        String[] daysOfWeek = {" Monday", "Tuesday", "Wednesday", "chetverng", "Friday", "Saturday", "Sunday"};
+        //3
         String[] stringArray = new String[10];
-        stringArray[2]="Hello World!";
+        isVivod(stringArray);
+
+        //6
+        Integer[] arr3={333,14,5,6,27,81,99,10};
 
 
-        System.out.println(stringArray[2]);
-        //Объявите массив строк с идентификатором daysOfWeek  и проинициализируйте его названиями дней недели.
-                String[] daysOfWeek = {" Monday", "Tuesday", "Wednesday", "chetverng", "Friday", "Saturday", "Sunday"};
+        isVozrastanie(arr3);
+        isUbivanie(arr3);
+        int[] arr8 = {5, 6, 8, -10, 22, 15};
+isPisotive(arr8);
 
-        System.out.println("Today " + daysOfWeek[2]);
-//Заполнить массив из восьми элементов следующими значениями: первый элемент массива равен 37, второй — 0, третий — 50, четвертый — 46, пятый — 34, шестой — 46, седьмой — 0, восьмой —13.
-        Integer[] arr2 = new Integer[8];
-        arr2[0] = 37;
-        arr2[1] = 0;
-        arr2[2] = 50;
-        arr2[3] = 46;
-        arr2[4] = 34;
-        arr2[5] = 46;
-        arr2[6] = 0;
-        arr2[7] = 13;
-
-        Arrays.sort(arr2);
-        for (int i = 0; i < arr2.length; i++)
-            System.out.println("Массив по возрастанию индекса "+i + "= " + arr2[i]);
-        System.out.print("Массив по убыванию: ");
-        Integer arr3[] = new Integer[10];
-        Arrays.sort(arr2, Collections.reverseOrder());
-//Выводим отсортированный массив на консоль.
-        for (int i = 0; i < arr2.length; i++) {
-
-            System.out.print(arr2[i] + "  ");
-        }
-        System.out.println(" ");
-        System.out.println("-------------------------------------------------- ");
-
-//Дан массив целых чисел. Выяснить:
-             int[] arr8 = {5, 6, 8, -10, 22, 15};
-                if (arr8[3] >= 0)
-                    System.out.println("Является ли 3-й элемент массива положительным числом *"+true+"*");
-                else
-                    System.out.println("Является ли 3-й элемент массива положительным числом *"+false+"*");
-
-                int chetnoe = arr8[4] % 2;
-                if (chetnoe == 0)
-                    System.out.println("Является ли 4-й элемент массива четным числом *"+true+"*");
-                else
-                    System.out.println("Является ли 4-й элемент массива четным числом *"+false+"*");
-
-
-                if(arr8[3]>arr8[4])
-                    System.out.println("Число массива под индексом 3 больше,чем число под индексом 4");
-                else
-                    System.out.println("Число массива под индексом 4 больше, чем число под индексом 3");
-        String[][] s = {
+        String[][] s9 = {
                 {"a(0,0)", "a(0,1)"},
                 {"a(1,0)", "a(1,1)"},
-                {"a(2,0)", "a(1,1)"}
+                {"a(2,0)", "a(1,1)"}};
+        isTwoVivod(s9);
 
-        };
-
-        System.out.println("Вывести на экран элемент, расположенный в правом верхнем углу массива.= "+s[0][1]);
-
-        int[][] a = {{1, 2, 3},
-                {4, 0, 54,},
-        };
-        System.out.println("Вывести на экран элемент, расположенный в левом нижнем углу массива ="+a[1][0]);
-        System.out.println(" Вывести на экран элемент, расположенный в правом нижнем углу массива ="+s[0][1]);
-        System.out.println(" Вывести на экран элемент, расположенный в левом верхнем углу массива ="+s[0][0]);
-        int y=0;
-        int x=2;
-        System.out.println(" вывод на экран любого элемента второй строки массива="+a[1][x]);
-        System.out.println(" Вывода на экран любого элемента массива="+a[y][x]);
-            }
+        int[][] a10 = {{1, 2, 3},
+                {4, 0, 54,},};
+        isTwoVivod2(a10);
+        isRandom(a10);
+    }
 
 
+
+    public static void isVivod(String stringArray[]) {
+        //5
+        stringArray[2] = "Hello World!";
+        System.out.println(stringArray[2]);
+    }
+   public static void isVozrastanie(Integer arr3[]){
+       Arrays.sort(arr3);
+       for (int i = 0; i < arr3.length; i++)
+           System.out.println("Массив по возрастанию индекса " + i + "= " + arr3[i]);
+    }
+public static void isUbivanie(Integer arr3[]) {
+
+    Arrays.sort(arr3, Collections.reverseOrder());
+    for (int i = 0; i < arr3.length; i++) {
+
+        System.out.println("По убыванию: " + arr3[i] + "  ");
+    }
+}
+    public static void isPisotive(int arr8[]) {
+
+        if (arr8[3] >= 0)
+            System.out.println("Является ли 3-й элемент массива положительным числом *" + true + "*");
+        else
+            System.out.println("Является ли 3-й элемент массива положительным числом *" + false + "*");
+
+        int chetnoe = arr8[4] % 2;
+        if (chetnoe == 0)
+            System.out.println("Является ли 4-й элемент массива четным числом *" + true + "*");
+        else
+            System.out.println("Является ли 4-й элемент массива четным числом *" + false + "*");
+
+
+        if (arr8[3] > arr8[4])
+            System.out.println("Число массива под индексом 3 больше,чем число под индексом 4");
+        else
+            System.out.println("Число массива под индексом 4 больше, чем число под индексом 3");
+    }
+       public static void isTwoVivod(String s9[][]){
+           System.out.println("Вывести на экран элемент, расположенный в правом верхнем углу массива.= "+s9[0][1]);
         }
 
-
-
+        public static void isTwoVivod2(int a10[][]) {
+            System.out.println(" Вывести на экран элемент, расположенный в правом нижнем углу массива =" + a10[0][1]);
+            System.out.println(" Вывести на экран элемент, расположенный в левом верхнем углу массива =" + a10[0][0]);
+        }
+            public static void isRandom(int a10[][]){
+        int y=0;
+        int x=2;
+        System.out.println(" вывод на экран любого элемента второй строки массива="+a10[1][x]);
+        System.out.println(" Вывода на экран любого элемента массива="+a10[y][x]);
+    }
+        }
 
 
 
