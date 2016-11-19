3bbfc17daa94ded7e58b0d8aa643b8afeaf8d9e0
DmitriyKuzmenko22 <dp190792kdg@gmail.com>
2016-11-19 16:34:08 +0200

    Update Сow.java

--- Сow.java
+++ Сow.java
@@ -7,21 +7,25 @@ public class Сow extends Mammal{
     }
 
     boolean hoof;
+boolean horns;
 
-
-    public Сow(boolean vegetarian, String eats, int noOfLegs,boolean hoof ) {
+    protected Сow(boolean vegetarian, String eats, int noOfLegs,boolean hoof ) {
         super(vegetarian, eats, noOfLegs);
-
         this.hoof=hoof;
     }
 
+    protected Сow(boolean vegetarian, String eats, int noOfLegs, boolean hoof, boolean horns) {
+        super(vegetarian, "", noOfLegs);
+        this.hoof = hoof;
+        this.horns = horns;
+    }
 
     public String getCow(){
+
         return "*. Есть копыта? -*"+hoof+"*";
     }
 
     public Сow(int noOfLegs,boolean hoof){
-
         this.hoof=hoof;
     }
 }
